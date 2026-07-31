8af6ccd410df6d99b8131c507d5873fbc0e0e808
scaramouche <skillsnwokolo372@gmail.com>
2026-07-30 23:42:10 -0230

    (fix): fixed images path

--- src/app/landing/pages/LandingHomePage/HeroSection/HeroSectionImages/api/hero-images.mock.ts
+++ src/app/landing/pages/LandingHomePage/HeroSection/HeroSectionImages/api/hero-images.mock.ts
@@ -4,37 +4,37 @@ import type { HeroImage } from "../types";
 const IMAGES: HeroImage[] = [
     {
         id: 1,
-        url: "/public/hero/IMG_6498.jpg",
+        url: "/images/hero/IMG_6498.jpg",
         alt_text: "Welcome to our church"
     },
     {
         id: 2,
-        url: "/public/hero/IMG_0194.jpg",
+        url: "/images/hero/IMG_0194.jpg",
         alt_text: "Welcome to our church"
     },
     {
         id: 3,
-        url: "/public/hero/IMG_0247.jpg",
+        url: "/images/hero/IMG_0247.jpg",
         alt_text: "Welcome to our church"
     },
     {
         id: 4,
-        url: "/public/hero/IMG_0364.jpg",
+        url: "/images/hero/IMG_0364.jpg",
         alt_text: "Welcome to our church"
     },
     {
         id: 5,
-        url: "/public/hero/IMG_9882.jpg",
+        url: "/images/hero/IMG_9882.jpg",
         alt_text: "Welcome to our church"
     },
     {
         id: 6,
-        url: "/public/hero/IMG_9955.jpg",
+        url: "/images/hero/IMG_9955.jpg",
         alt_text: "Welcome to our church"
     },
     {
         id: 7,
-        url: "/public/hero/IMG_9956.jpg",
+        url: "/images/hero/IMG_9956.jpg",
         alt_text: "Sunday worship"
     },
 ]
